f3d93d3655bd2b7af1aa17203dbadc994f6eddc5
Sergei <sergei.visotsky@gmail.com>
2018-08-15 16:54:11 +0300

    Initial commit

---
+++ src/main/java/org/sergei/jenkins/showcase/controller/Welcome.java
@@ -0,0 +1,17 @@
+package org.sergei.jenkins.showcase.controller;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Welcome extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req,
+                         HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.write("Welcome Jenkins!");
+    }
+}
